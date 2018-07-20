94971165c781349a0a1da78f8c18b962f23ae62e
Ivan Fefer <ivan.fefer@aimtech.com>
2018-07-20 14:24:38 +0300

    fix deploy script

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 dotfiles=(".bashrc" ".screenrc" ".vim" ".vimrc")
-dir="${HOME}/.dotfiles"
+dir="${HOME}/dotfiles"
 
 for dotfile in "${dotfiles[@]}"; do
   ln -s "${dir}/${dotfile}" "${HOME}/${dotfile}"

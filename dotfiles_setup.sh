351952724cb91e44f40a61d47944a97a17e02fe5
Ivan Fefer <fefer.ivan@gmail.com>
2019-08-12 18:49:34 +0300

    add force

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -3,6 +3,6 @@ dotfiles=(".bashrc" ".screenrc" ".tmux.conf" ".vim" ".vimrc")
 dir="${HOME}/dotfiles"
 
 for dotfile in "${dotfiles[@]}"; do
-  ln -s "${dir}/${dotfile}" "${HOME}/${dotfile}"
+  ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}"
 done
 
